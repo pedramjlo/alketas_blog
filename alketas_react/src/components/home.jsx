2126713fe3630f5631b7d627f235ce5848949142
pedramjlo <pedram_sif@tuta.io>
2024-08-19 17:43:57 +0330

    split the screen

--- alketas_react/src/components/home.jsx
+++ alketas_react/src/components/home.jsx
@@ -2,11 +2,20 @@ import React from 'react';
 
 import Navbar from './navbar.jsx';
 
+import '../style/home.css';
+
 
 const HomePage = () => {
     return(
         <div className='home-container'>
             <Navbar />
+
+            <div class="container">
+                <div class="left">Left</div>
+                <div class="middle">Middle</div>
+                <div class="right">Right</div>
+            </div>
+
         </div>
     );
 };
