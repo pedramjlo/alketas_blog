840f8ae3b37f8fc9f8290a70cf338dcd943db6c4
pedramjlo <pedram_sif@tuta.io>
2024-08-19 19:04:36 +0330

    created login page

--- alketas_react/src/components/user_account/login.jsx
+++ alketas_react/src/components/user_account/login.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 
 import Navbar from '../navbar';
 
@@ -7,12 +7,64 @@ import '../../style/user_account/login.css';
 
 
 const LoginPage = () => {
-    const [username, SetUsername] = useState('');
-    const [password, SetPassword] = useState('');
+    const [username, setUsername] = useState('');
+    const [password, setPassword] = useState('');
+    const [errors, setErrors] = useState('');
+
+
+    const handleLogin = async (event) => {
+        event.preventDefault();
+        setErrors(null);
+
+        try {
+            const response = await fetch('http://127.0.0.1:8000/login', {
+                method: 'Post',
+                headers: {
+                    'Content-Type': 'application/json'
+                },
+                body: JSON.stringify({ username, password}),
+            });
+
+            // handle unsuccessful login
+            if (!response.status == 202) {
+                throw new Error(response.data.message);
+            }
+
+            const data = await response.json();
+            console.log('successful login')
+
+        } catch (error) {
+            setErrors(error.message)
+        }
+
+    };
     
     return (
-        <div>
+        <div className='form-page'>  
+           <div className='form-container'>
+                <h2>ورود</h2>
+                <form onSubmit={handleLogin}>
+                    <label>نام کاربری</label>
+                    <input 
+                        type="text"
+                        value={username}
+                        required
+                    />
+                    <label>گذرواژه</label>
+                    <input 
+                        type="text"
+                        value={password}
+                        required
+                    />  
+                    <button type='submit'>ورود</button>
+                    {error && <p>{error}</p>}
+                </form>
 
+           </div>
         </div>
     );
-};
+};  
+
+
+
+export default LoginPage;
