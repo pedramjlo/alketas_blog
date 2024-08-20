087d8763662546b3fc12fe977a4e2e9d114226e3
pedramjlo <pedram_sif@tuta.io>
2024-08-20 20:21:01 +0330

    added icons to homepage

--- alketas_react/src/components/home.jsx
+++ alketas_react/src/components/home.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import Navbar from './navbar';
+import Icons from './icons';
 
 
 import '../styles/home.css';
@@ -20,6 +21,7 @@ const HomePage = () => {
 
                     <div className='home-description'>
                         <p>به پروژه ی فول استک من خوش آمدید. من پدرام جلالی این وب اپلیکیشن فول اسنک نوشته شده با جنگو و ری اکت را صرفا برای رزومه شخصی خودم درست کرده ام. آلکتاس یک اب CRUD است که ری اکت از طریق متود fetch با اند پوینت های ویوهای DRF ارتباط برقرار میکند</p>
+                        <Icons />
                     </div>
                 </div>
 
--- alketas_react/src/components/icons.jsx
+++ alketas_react/src/components/icons.jsx
@@ -1,10 +1,18 @@
 import React from 'react';
 
+import '../styles/icons/icons.css';
+
 
 const Icons = () => {
     return (
-        <div className=''>
-
+        <div className='project-icons'>
+            <h3>Technologies used in this project</h3>
+            <div className='icons'>
+                <img src="/icons/django.svg" alt="Django"/>
+                <img src="/icons/python.svg" alt="Django"/>
+                <img src="/icons/bootstrap.svg" alt="Django"/>
+                <img src="/icons/react.svg" alt="Django"/>
+            </div>
         </div>
     );
 };
