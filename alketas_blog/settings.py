200f8edc15e04a55d7327f96133997f6c91765ee
pedramjlo <pedram_sif@tuta.io>
2024-08-23 19:19:07 +0330

    removed secret key

--- alketas_blog/settings.py
+++ alketas_blog/settings.py
@@ -1,6 +1,10 @@
 import os
 from pathlib import Path
 
+from decouple import config
+
+SECRET_KEY = config('SECRET_KEY')
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -9,7 +13,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-)$qk=kw%w&-2y&i2xas9mbpnmu1ek$_i7v_w4%t(!g474!nh%1'
+
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
