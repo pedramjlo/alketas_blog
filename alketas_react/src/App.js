9099b0a43c5ff7bc971aabb1967a614f1c6c93ba
pedramjlo <pedram_sif@tuta.io>
2024-08-26 00:13:04 +0330

    before leaving

--- alketas_react/src/App.js
+++ alketas_react/src/App.js
@@ -3,12 +3,14 @@ import './App.css';
 
 import HomePage from './components/home';
 import FeedPage from './components/blog/feed';
+import LoginPage from './components/user_account/login';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
 function App() {
   return (
     <Router>
       <Routes>
+        <Route path="/login" element={<LoginPage />} />
         <Route path="/feed" element={<FeedPage />} />
         <Route path="/" element={<HomePage />} />
       </Routes>
---
+++ alketas_react/src/components/user_account/login.jsx
@@ -0,0 +1,71 @@
+import React, { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
+import Navbar from '../navbar';
+
+import '../../styles/user_account/login.css';
+
+const LoginPage = () => {
+    const [username, setUsername] = useState('');
+    const [password, setPassword] = useState('');
+    const [error, setError] = useState('');
+    const navigate = useNavigate();
+
+    const handleLogin = async (event) => {
+        event.preventDefault(); // Prevent the default form submission
+
+        try {
+            const response = await fetch('http://localhost:8000/api/login', {
+                method: "POST",
+                headers: {
+                    "Content-Type": "application/json",
+                },
+                body: JSON.stringify({ username, password })
+            });
+
+            const data = await response.json();
+
+            if (data.success) {
+                navigate('/');
+            } else {
+                setError('Invalid Credentials!');
+            }
+        } catch (error) {
+            setError('An error occurred. Please try again.');
+        }
+    };
+
+    return (
+        <div className='login-page'>
+            <Navbar />
+                <div className='form-container'>
+                    <div className='form'>
+                    <h2>ورود</h2>
+                    <form onSubmit={handleLogin}>
+                        <div className='form-username'>
+                            <label>نام کاربری</label>
+                            <input
+                                type="text"
+                                value={username}
+                                onChange={(e) => setUsername(e.target.value)}
+                                required
+                            />
+                        </div>
+                        <div className='form-password'>
+                            <label>رمز عبور</label>
+                            <input
+                                type="password"
+                                value={password}
+                                onChange={(e) => setPassword(e.target.value)}
+                                required
+                            />
+                        </div>
+                        <button type="submit">Login</button>
+                        {error && <p>{error}</p>}
+                    </form>
+                    </div>
+                </div>
+        </div>
+    );
+};
+
+export default LoginPage;
