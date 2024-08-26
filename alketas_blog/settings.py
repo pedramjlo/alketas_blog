50495b7038b0f2c032b2e2ab5191261940b36634
pedramjlo <pedram_sif@tuta.io>
2024-08-26 14:07:50 +0330

    separated react

--- alketas_blog/settings.py
+++ alketas_blog/settings.py
@@ -129,11 +129,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
 STATIC_URL = '/static/'
+STATICFILES_DIRS = []
 
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'static'),
-]
-
+# Media files
 
 
 # Default primary key field type
--- alketas_blog/urls.py
+++ alketas_blog/urls.py
@@ -4,6 +4,8 @@ from django.urls import path, include
 
 
 
+
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include('user_account.urls')),
